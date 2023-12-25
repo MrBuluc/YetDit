67288cbc68744622752f6c25f752ed31775f6d2c
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-12-25 11:47:29 +0300

    [Feat] Abstractions IUserService

---
+++ Core/YetGit.Application/Abstractions/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using YetGit.Application.DTOs.User;
+
+namespace YetGit.Application.Abstractions.Services
+{
+    public interface IUserService
+    {
+        Task<CreateUserResponse> CreateAsync(CreateUser user);
+    }
+}
