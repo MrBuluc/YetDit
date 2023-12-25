bbb978d759b5b88be8d2ce9d1a5157954fc3b5e9
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-12-25 17:02:07 +0300

    [Feat] UpdatePostCommand

---
+++ Core/YetDit.Application/Features/Commands/Post/UpdatePost/UpdatePostCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using YetDit.Application.Repositories.Post;
+
+namespace YetDit.Application.Features.Commands.Post.UpdatePost
+{
+    public class UpdatePostCommandHandler : IRequestHandler<UpdatePostCommandRequest, UpdatePostCommandResponse>
+    {
+        private readonly IPostReadRepository _readRepository;
+        private readonly IPostWriteRepository _writeRepository;
+
+        public UpdatePostCommandHandler(IPostReadRepository readRepository, IPostWriteRepository writeRepository)
+        {
+            _readRepository = readRepository;
+            _writeRepository = writeRepository;
+        }
+        public async Task<UpdatePostCommandResponse> Handle(UpdatePostCommandRequest request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.Post post = await _readRepository.GetByIdAsync(request.Id);
+            post.Title = request.Title;
+            post.Description = request.Description;
+            post.ModifiedOn = DateTime.UtcNow;
+            post.ModifiedByUserId = request.UserId;
+            await _writeRepository.SaveAsync();
+            return new()
+            {
+                Succeeded = true,
+            };
+        }
+    }
+}
---
+++ Core/YetDit.Application/Features/Commands/Post/UpdatePost/UpdatePostCommandResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace YetDit.Application.Features.Commands.Post.UpdatePost
+{
+    public class UpdatePostCommandResponse
+    {
+        public bool Succeeded { get; set; }
+    }
+}
