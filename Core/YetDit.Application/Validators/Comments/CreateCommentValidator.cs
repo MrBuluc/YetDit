d07c186c49cd2579924358e21b54d1334e011b28
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-12-30 14:10:55 +0300

    [Feat] CreateCommentValidator

---
+++ Core/YetDit.Application/Validators/Comments/CreateCommentValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using YetDit.Application.ViewModels.Comments;
+
+namespace YetDit.Application.Validators.Comments
+{
+    public class CreateCommentValidator : AbstractValidator<VM_Create_Comment>
+    {
+        public CreateCommentValidator()
+        {
+            RuleFor(c => c.Content)
+                .NotEmpty()
+                .WithMessage("Can't accept empty content value!")
+                .MaximumLength(3)
+                .WithMessage("Please enter a value that is greater than 3 char!");
+        }
+    }
+}
---
+++ Core/YetDit.Application/ViewModels/Comments/VM_Create_Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace YetDit.Application.ViewModels.Comments
+{
+    public class VM_Create_Comment
+    {
+        public string Content { get; set; }
+        public string UserId { get; set; }
+        public int PostId { get; set; }
+    }
+}
