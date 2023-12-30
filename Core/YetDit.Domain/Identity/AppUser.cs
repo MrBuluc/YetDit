49b39bd780ca04bbe448fe633816c2e29ba06e17
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-12-30 14:04:16 +0300

    [Feat] CommentConfiguration

--- Core/YetDit.Domain/Identity/AppUser.cs
+++ Core/YetDit.Domain/Identity/AppUser.cs
@@ -21,5 +21,6 @@ namespace YetDit.Domain.Identity
         public bool IsDeleted { get; set; }
 
         public ICollection<Post> Posts { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
