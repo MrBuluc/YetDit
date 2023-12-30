12a113ba2ba017a8d96ad8a304a1fae0b9772867
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-12-30 14:39:39 +0300

    [Feat] YetDitDbContextFactory

---
+++ Infrastructure/YetDit.Persistence/Contexts/YetDitDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace YetDit.Persistence.Contexts
+{
+    public class YetDitDbContextFactory : IDesignTimeDbContextFactory<YetDitDbContext>
+    {
+        public YetDitDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<YetDitDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new YetDitDbContext(dbContextOptionsBuilder.Options);
+        }
+    }
+}
