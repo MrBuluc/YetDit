53945653614f1ad610802c68dd25dd58dc204b13
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-12-30 12:46:41 +0300

    [Feat] CreatePostValidator

---
+++ Core/YetDit.Application/Validators/Posts/CreatePostValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using YetDit.Application.ViewModels.Posts;
+
+namespace YetDit.Application.Validators.Posts
+{
+    public class CreatePostValidator : AbstractValidator<VM_Create_Post>
+    {
+        public CreatePostValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .WithMessage("Can't accept empty title value!")
+                .MinimumLength(3)
+                .WithMessage("Please enter a value that is greater than 3 char!");
+
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .WithMessage("Can't accept empty description value!")
+                .MinimumLength(3)
+                .WithMessage("Please enter a value that is greater than 3 char!");
+        }
+    }
+}
---
+++ Core/YetDit.Application/ViewModels/Posts/VM_Create_Post.cs
@@ -0,0 +1,9 @@
+﻿namespace YetDit.Application.ViewModels.Posts
+{
+    public class VM_Create_Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+    }
+}
