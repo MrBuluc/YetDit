3ccc8de9d8b43860d7cdf9d4f4fd3aa275fd5558
Hakkıcan Bülüç <43816007+MrBuluc@users.noreply.github.com>
2023-12-30 12:36:06 +0300

    [Feat] IncrementUpVoteCountCommentCommand

---
+++ Core/YetDit.Application/Features/Commands/Comment/IncrementUpVoteCountComment/IncrementUpVoteCountCommentCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using YetDit.Application.Repositories.Comment;
+
+namespace YetDit.Application.Features.Commands.Comment.IncrementUpVoteCountComment
+{
+    public class IncrementUpVoteCountCommentCommandHandler : IRequestHandler<IncrementUpVoteCountCommentCommandRequest, IncrementUpVoteCountCommentCommandResponse>
+    {
+        private readonly ICommentReadRepository _readRepository;
+        private readonly ICommentWriteRepository _writeRepository;
+
+        public IncrementUpVoteCountCommentCommandHandler(ICommentWriteRepository writeRepository, ICommentReadRepository readRepository)
+        {
+            _writeRepository = writeRepository;
+            _readRepository = readRepository;
+        }
+
+        public async Task<IncrementUpVoteCountCommentCommandResponse> Handle(IncrementUpVoteCountCommentCommandRequest request, CancellationToken cancellationToken)
+        {
+            Domain.Entities.Comment comment = await _readRepository.GetByIdAsync(request.Id);
+            if (!comment.IsDeleted)
+            {
+                comment.UpVoteCount++;
+                comment.ModifiedByUserId = request.UserId;
+                await _writeRepository.SaveAsync();
+                return new()
+                {
+                    Succeeded = true,
+                    NewUpVoteCount = comment.UpVoteCount,
+                };
+
+            }
+            return new() { Succeeded = false, };
+        }
+    }
+}
---
+++ Core/YetDit.Application/Features/Commands/Comment/IncrementUpVoteCountComment/IncrementUpVoteCountCommentCommandResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace YetDit.Application.Features.Commands.Comment.IncrementUpVoteCountComment
+{
+    public class IncrementUpVoteCountCommentCommandResponse
+    {
+        public bool Succeeded { get; set; }
+        public int NewUpVoteCount { get; set; }
+    }
+}
